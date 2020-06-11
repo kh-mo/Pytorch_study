2cb865e7d6486394f7c9b7ff81f2b4e37501a87d
khmo <momkdh2@naver.com>
2020-06-11 15:23:55 +0900

    add 2 reference content(error1,2 and metaclass)

--- abc_tutorial.py
+++ abc_tutorial.py
@@ -1,40 +1,39 @@
 '''
-ABC 클래스는 ABC 클래스를 상속받은 클래스(A)를 추후 다른 클래스가(B)가 상속받을 때,
-A 클래스의 메서드를 반드시 구현하도록 강제하는 추상화 클래스다.
+ABC 클래스를 메타클래스로 지정해서 만든 클래스는 추상 클래스가 된다
+    - 추상 클래스는 인스턴스로 만들 수 없다
+    - @abstractmethod 데코레이터를 사용해서 반드시 구현해야 하는 메소드를 정의할 수 있다
 
 Q1 : __metaclass__란?
+metaclass를 나타낸다
+metaclass란 클래스의 클래스다
+python에서 클래스는 객체다
+객체를 만드는 것이 클래스이기 때문에 클래스를 만들기 위한 클래스가 존재하고 이것이 메타클래스다
+파이썬이 사용하는 내장된 메타클래스는 type이다
 '''
 
-import abc
+from abc import *
 
-class base:
-
-    __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
-    def func1(self):
+class base(metaclass=ABCMeta):
+    @abstractmethod
+    def study(self):
+        pass
+    @abstractmethod
+    def g2s(self):
         pass
 
-    @abc.abstractmethod
-    def func2(self):
-        pass
+class student(base):
+    def study(self):
+        print("study")
 
-class a(base):
-    def func1(self):
-        print("func1 a")
-    def func2(self):
-        print("func2 a")
+# 추상 클래스는 인스턴스로 만들 수 없다
+mosi = sb()
 
-class b(base):
-    def func1(self):
-        print("func1 b")
-    # def func2(self):
-    #     print("func2 b")
+# @abstractmethod 데코레이터를 사용해서 반드시 구현해야 하는 메소드를 정의할 수 있다
+mosi = student()
 
-ma = a()
-mb = b()
+'''
+reference
+1. https://dojang.io/mod/page/view.php?id=2389
+2. https://tech.ssut.me/understanding-python-metaclasses/
+'''
 
-ma.func1()
-ma.func2()
-mb.func1()
-mb.func2()
